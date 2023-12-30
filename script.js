e2378f76fc1e07d34966e45ae9eaa9df2e8b3622
pasxtsit <123456789+odin@users.noreply.github.com>
2023-12-30 20:05:56 +0200

    element target

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let displayCurrent = document.querySelector(".displayCurrent");
+let displayPrevious = document.querySelector(".displayPrevious");
+let displayResult = document.querySelector(".displayResult");
+const buttons = document.querySelectorAll('.num');
+const opButtons = document.querySelectorAll('.op');
+const btnC = document.querySelector(".C");
+const btnDel = document.querySelector(".Del");
+const btnEq = document.querySelector(".eq");
+const btnFlo = document.querySelector(".flo");
